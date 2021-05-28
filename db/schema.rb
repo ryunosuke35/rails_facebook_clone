b38880c132cf59f94d35dad53321774e128f9ecd
ryunosuke35 <6bev2103@gmail.com>
2021-05-28 10:35:31 +0900

    画像と文章の投稿機能を実装

--- db/schema.rb
+++ db/schema.rb
@@ -1,6 +1,21 @@
-ActiveRecord::Schema.define(version: 2021_05_27_030835) do
+ActiveRecord::Schema.define(version: 2021_05_27_153253) do
+
 
   enable_extension "plpgsql"
+
+  create_table "feeds", force: :cascade do |t|
+    t.text "image"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+
+
+
+
+
+
 
   create_table "users", force: :cascade do |t|
     t.string "name"
